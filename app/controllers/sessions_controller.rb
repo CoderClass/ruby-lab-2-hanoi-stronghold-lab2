9fb395f9284e84620fe2b2b4c5e9bd215607d1e7
Manh T. Truong <manhattan.vn@gmail.com>
2016-10-18 23:32:25 +0700

    Milestone 3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,23 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by_email params[:email]
-    if @user.authenticate(params[:password]) 
-      session[:user_id] = @user.id
-      flash[:succes] = 'Login successfully'
-      redirect_to users_path
+    if @user
+      if @user.authenticate(params[:password]) 
+        session[:user_id] = @user.id
+        flash[:success] = 'Login successfully'
+        redirect_to users_path
+      else
+        flash[:error] = 'Password is not correct'
+        redirect_to login_path
+      end 
     else
-      flash[:error] = 'User or password is not correct'
-      redirect_to login_path
-    end 
+      flash[:error] = 'User not found'
+      redirect_to login_path 
+    end
+  end
 
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
